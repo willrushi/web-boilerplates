d2131ad2a7d07fdf76797a9b8a105d50729b9250
Will Roberts <willprobertss@gmail.com>
2021-12-12 17:00:29 +0000

    python

---
+++ python/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from route.blueprints import routes
+
+app = Flask(__name__, static_url_path='/assets', static_folder='assets')
+
+# change this!
+app.secret_key = "965caf956f95a7928345abafcb9f156"
+
+app.register_blueprint(routes)
+
+
+if __name__ == '__main__':
+    app.run(debug=True,  host='0.0.0.0')
