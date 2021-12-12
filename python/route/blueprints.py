e99d7a751dbb91dc23b7f2aa7cf9726c1ec3278e
Will Roberts <willprobertss@gmail.com>
2021-12-12 17:04:03 +0000

    params

--- python/route/blueprints.py
+++ python/route/blueprints.py
@@ -5,3 +5,7 @@ routes = Blueprint("routes", __name__, template_folder="templates")
 @routes.route("/")
 def index():
     return render_template("index.html")
+
+@routes.route("/<name>")
+def params(name):
+    return render_template("index.html", name=name)
